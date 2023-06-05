00826cb168edbdd85eaf059b15142b22c311514f
michikawa07 <michikawa.ryohei@gmail.com>
2023-06-06 04:23:33 +0900

    small fix

--- src/EvalDataFrames.jl
+++ src/EvalDataFrames.jl
@@ -6,17 +6,16 @@ export eval!
 
 """
 	eval!(df::DataFrame, syms::AbstractArray; parser=Meta.parse, mod=Main)
-
-Unitfulパッケージによって提供される単位付き数値を
-DataFrame+CSVで保存・読み込みすることを念頭に作成したもの
-DataFrameの指定したカラムの文字列をjuliaの構文で評価する．
+	eval!(df::DataFrame, syms::InvertedIndex; parser=Meta.parse, mod=Main)
+	eval!(df::DataFrame, syms...            ; parser=Meta.parse, mod=Main)
+	eval!(df::DataFrame                     ; parser=Meta.parse, mod=Main)
 """
 function eval!(df::DataFrame, syms::AbstractArray; parser=Meta.parse, mod=Main)
 	for sym in syms
 		type=eltype(df[!, sym])
 		try
-			type <: AbstractString && (df[!,sym] = df[!, sym] .|> parser .|> mod.eval)
-			type <: Expr && (df[!,sym] = df[!, sym] .|> mod.eval)
+			type <: AbstractString && (@. df[!,sym] = df[!, sym] |> parser |> mod.eval)
+			type <: Expr           && (@. df[!,sym] = df[!, sym] |> mod.eval)
 		catch error;
 			type <: AbstractString && @warn "following string in the colmun '$sym' cannot be parse" error _file="line"
 			type <: AbstractString || @warn "the colmun '$sym' cannot be parse, because the type is $type" _file="line"
@@ -25,7 +24,7 @@ function eval!(df::DataFrame, syms::AbstractArray; parser=Meta.parse, mod=Main)
 	df
 end
 eval!(df::DataFrame, syms_inv::InvertedIndex; karg...) = eval!(df, propertynames(df[!,syms_inv]); karg...)
-eval!(df::DataFrame, syms...; karg...) = eval!(df, collect(syms); karg...)
-eval!(df::DataFrame; karg...) = eval!(df, propertynames(df); karg...)
+eval!(df::DataFrame, syms...                ; karg...) = eval!(df, collect(syms)                ; karg...)
+eval!(df::DataFrame                         ; karg...) = eval!(df, propertynames(df)            ; karg...)
 
 end
